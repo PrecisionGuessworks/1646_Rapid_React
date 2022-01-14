6c4203d256102cb395b2ab430a5f704428b9b686
cland29 <carl.lee.landskron@gmail.com>
2022-01-14 00:44:35 -0500

    Enforce TalonFXInversionType on TalonFXFactory

--- src/main/java/frc/robot/lib/TalonFXFactory.java
+++ src/main/java/frc/robot/lib/TalonFXFactory.java
@@ -3,6 +3,7 @@ package frc.robot.lib;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 public class TalonFXFactory {
@@ -34,7 +35,7 @@ public class TalonFXFactory {
    public static TalonFX makeFollowerTalonFX(int id, TalonFX leader){
       TalonFX talon = makeTalonFX(id);
       talon.set(ControlMode.Follower, leader.getDeviceID());
-      talon.setInverted(InvertType.FollowMaster);
+      talon.setInverted(TalonFXInvertType.FollowMaster);
       return talon;
    }
 }
