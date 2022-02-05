5241b62945dd1d4e23db945d9c6cff10f8e5982c
cland29 <carl.lee.landskron@gmail.com>
2022-02-05 13:00:03 -0500

    Create Intake class

---
+++ src/main/java/frc/robot/subsystems/Intake/IntakeSubsystem.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.Intake;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IntakeSubsystem extends SubsystemBase {
+
+  private IntakeSubsystem instance;
+
+  private VictorSPX leftIntakeMotor, rightIntakeMotor;
+
+  private IntakeSubsystem() {
+    leftIntakeMotor = new VictorSPX(10);
+    rightIntakeMotor = new VictorSPX(11);
+
+    rightIntakeMotor.setInverted(InvertType.InvertMotorOutput);
+  }
+
+  public IntakeSubsystem getInstance(){
+    if (instance == null){
+      instance = new IntakeSubsystem();
+    }
+    return instance;
+  }
+
+  public void setPower(double power){
+    leftIntakeMotor.set(ControlMode.PercentOutput, power);
+    rightIntakeMotor.set(ControlMode.PercentOutput, power);
+  }
+
+  @Override
+  public void periodic() {
+  }
+}
