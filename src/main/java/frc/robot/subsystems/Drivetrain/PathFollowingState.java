2b0be2bf35f0a86eb6e09c1e3842aac82ffe1fd5
cland29 <carl.lee.landskron@gmail.com>
2022-02-26 16:47:34 -0500

    add path following state

---
+++ src/main/java/frc/robot/subsystems/Drivetrain/PathFollowingState.java
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.Drivetrain;
+
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class PathFollowingState extends CommandBase {
+  DrivetrainSubsystem drive = DrivetrainSubsystem.getInstance();
+  Trajectory pathToFollow;
+  Timer pathTimer;
+
+  public PathFollowingState(Trajectory pathToFollow) {
+    addRequirements(drive);
+    this.pathToFollow = pathToFollow;
+    pathTimer = new Timer();
+  }
+
+
+
+  
+  @Override
+  public void initialize() {
+    pathTimer.start();
+  }
+
+  @Override
+  public void execute() {
+    double currentTime = pathTimer.get();
+    Trajectory.State currentState = pathToFollow.sample(currentTime);
+
+    double velocity = currentState.velocityMetersPerSecond;
+    double angularVelocity = currentState.curvatureRadPerMeter * velocity;
+
+    double leftSpeed = velocity - angularVelocity;
+    double rightSpeed = velocity - angularVelocity;
+    drive.setSpeed(leftSpeed, rightSpeed);
+
+
+
+  }
+
+
+  @Override
+  public boolean isFinished() {
+    return pathTimer.hasElapsed(pathToFollow.getTotalTimeSeconds());
+  }
+}
