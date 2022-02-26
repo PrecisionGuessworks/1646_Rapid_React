e2cdd3245ac00f37e645e5eec85a81bf28adf419
cland29 <carl.lee.landskron@gmail.com>
2022-02-26 15:27:17 -0500

    add drivebackwards state

---
+++ src/main/java/frc/robot/subsystems/Drivetrain/states/DriveBackwards.java
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.Drivetrain.states;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain.DrivetrainSubsystem;
+
+public class DriveBackwards extends CommandBase {
+  DrivetrainSubsystem drive = DrivetrainSubsystem.getInstance();
+  public DriveBackwards() {
+    addRequirements(drive);
+  }
+
+  @Override
+  public void initialize() {
+    drive.curvatureDrive(-0.5, 0);
+  }
+
+}
