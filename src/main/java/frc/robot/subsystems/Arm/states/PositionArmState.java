589e9f39e6bbde773d740fa18c8610bedb651dec
cland29 <carl.lee.landskron@gmail.com>
2022-03-22 16:59:46 -0400

    Remove unnecessary comments

--- src/main/java/frc/robot/subsystems/Arm/states/PositionArmState.java
+++ src/main/java/frc/robot/subsystems/Arm/states/PositionArmState.java
@@ -18,11 +18,9 @@ public class PositionArmState extends CommandBase {
     this.position = position;
   }
 
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     if (position == ArmPosition.HIGH){
@@ -34,11 +32,9 @@ public class PositionArmState extends CommandBase {
 
   }
 
-  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {}
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
